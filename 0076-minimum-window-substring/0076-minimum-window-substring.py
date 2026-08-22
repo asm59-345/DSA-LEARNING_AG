4eb1493733f35edadbec13c11a4e200ed786e7c6
Ashmit Gautam <ashmit4933@gmail.com>
2026-08-22 23:14:29 +0530

    Time: 151 ms (19.41%), Space: 19.8 MB (11.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,36 +1,29 @@
 from collections import Counter
-                                                     
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t:
-            return ""
-
         need = Counter(t)
-        window = {}
+        window = Counter()
 
         left = 0
         count = 0
-        min_len = float("inf")
-        answer = ""
+        ans = ""
 
         for right in range(len(s)):
-            ch = s[right]
-            window[ch] = window.get(ch, 0) + 1
+            window[s[right]] += 1
 
-            if ch in need and window[ch] <= need[ch]:
+            if window[s[right]] <= need[s[right]]:
                 count += 1
 
             while count == len(t):
-                if right - left + 1 < min_len:
-                    min_len = right - left + 1
-                    answer = s[left:right + 1]
+                if not ans or right - left + 1 < len(ans):
+                    ans = s[left:right + 1]
 
-                left_ch = s[left]
-                window[left_ch] -= 1
+                window[s[left]] -= 1
 
-                if left_ch in need and window[left_ch] < need[left_ch]:
+                if window[s[left]] < need[s[left]]:
                     count -= 1
 
                 left += 1
 
-        return answer
+        return ans
