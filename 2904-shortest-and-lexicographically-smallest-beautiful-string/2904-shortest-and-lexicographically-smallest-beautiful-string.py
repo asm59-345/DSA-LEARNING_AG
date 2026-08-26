0efc18498011a49bc3fb165c891c7575b82d8796
Ashmit Gautam <ashmit4933@gmail.com>
2026-08-26 10:57:31 +0530

    Time: 0 ms (100%), Space: 19.4 MB (30.63%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -2,12 +2,32 @@ class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
         n = len(s)
         ans = ""
+        min_len = n + 1
+        
+        count = 0
+        left = 0
+        
+        for right in range(n):
+            if s[right] == '1':
+                count += 1
 
-        for i in range(n):
-            for j in range(i+1, n +1):
-                sub = s[i:j]
-                if sub.count('1') == k:
+            while count == k:
 
-                    if not ans or len(sub) < len(ans) or (len(sub) == len(ans) and sub < ans):
-                        ans = sub
+                while s[left] == '0':
+                    left += 1
+                
+                curr_sub = s[left : right + 1]
+                curr_len = len(curr_sub)
+                
+             
+                if curr_len < min_len:
+                    min_len = curr_len
+                    ans = curr_sub
+                elif curr_len == min_len:
+                    ans = min(ans, curr_sub)
+
+                if s[left] == '1':
+                    count -= 1
+                left += 1
+                
         return ans
