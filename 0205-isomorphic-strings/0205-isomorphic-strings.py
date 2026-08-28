722b991aecac55f9b0d097cb133d10206f6986b3
Ashmit Gautam <ashmit4933@gmail.com>
2026-08-29 00:06:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (22.54%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,20 +1,14 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-
-        if len(s) != len(t):
-            return False
-
-        map_s_to_t = {}
-        map_t_to_s = {}
-
-        for char_s, char_t in zip(s, t):
-            if char_s in map_s_to_t and map_s_to_t[char_s] != char_t:
+        mapping = {}
+        
+        for c1, c2 in zip(s, t):
+            if c1 in mapping:
+                if mapping[c1] != c2:
+                    return False
+            elif c2 in mapping.values():  
                 return False
-
-            if char_t in map_t_to_s and map_t_to_s[char_t] != char_s:
-                return False
-
-            map_s_to_t[char_s] = char_t
-            map_t_to_s[char_t] = char_s
-
+            else:
+                mapping[c1] = c2
+                
         return True
